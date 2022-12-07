93ed4da9fa6590b53693d30e13607023396852e7
cshier-capu <120051950+cshier-capu@users.noreply.github.com>
2022-12-07 11:54:21 -0800

    Update appendThumbs.user.js
    added console log diagnostic output

--- appendThumbs.user.js
+++ appendThumbs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add thumbnails
 // @namespace    https://service.capilanou.ca/
-// @version      0.3.1
+// @version      0.3.2
 // @description  Adds images next to View links in Attachment panel
 // @author       Chris Shier
 // @match        https://service.capilanou.ca/TDNext/Apps/72/Tickets/TicketDet?TicketID=*
@@ -13,8 +13,9 @@
 // ==/UserScript==
 (function() {
     'use strict';
-
+    console.log('appendThumbs userscript loaded')
     function appendThumbs() {
+        console.log('appendThumbs function fired')
         let view_links = document.querySelectorAll("a[title='View file']")
         for (let i = 0; i < view_links.length; i++) {
             let thumb = document.createElement('img');
