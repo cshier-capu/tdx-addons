8fee14a4dd35851a1292d3846a10489f2f81aa04
cshier-capu <120051950+cshier-capu@users.noreply.github.com>
2022-12-07 10:55:15 -0800

    Update appendThumbs.js
    add downloadURL

--- appendThumbs.js
+++ appendThumbs.js
@@ -7,6 +7,7 @@
 // @match        https://service.capilanou.ca/TDNext/Apps/72/Tickets/TicketDet?TicketID=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=capilanou.ca
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/cshier-capu/tdx-addons/main/appendThumbs.js
 // ==/UserScript==
 (function() {
     'use strict';
