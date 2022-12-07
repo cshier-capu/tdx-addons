f309129b836b5e82382f47fe5b1cdcd5d563d96e
cshier-capu <120051950+cshier-capu@users.noreply.github.com>
2022-12-07 10:51:12 -0800

    Create appendThumbs.js
    initial commit

---
+++ appendThumbs.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Add thumbnails
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds images next to View links in Attachment panel
+// @author       You
+// @match        https://service.capilanou.ca/TDNext/Apps/72/Tickets/TicketDet?TicketID=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=capilanou.ca
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    function appendThumbs() {
+        let view_links = document.querySelectorAll("a[title='View file']")
+        for (let i = 0; i < view_links.length; i++) {
+            let thumb = document.createElement('img');
+            thumb.src = view_links[i].href;
+            thumb.maxWidth = thumb.maxHeight = 300
+            view_links[i].appendChild(thumb);
+        }
+    }
+    window.addEventListener('load', appendThumbs, false)
+})();
