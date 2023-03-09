5b0d24c910504d7000f01599bd1605028e3250fe
cshier-capu <120051950+cshier-capu@users.noreply.github.com>
2023-03-09 12:24:21 -0800

    Update appendThumbs.user.js
    Change URL match following TDX url change

--- appendThumbs.user.js
+++ appendThumbs.user.js
@@ -4,7 +4,7 @@
 // @version      0.3.2
 // @description  Adds images next to View links in Attachment panel
 // @author       Chris Shier
-// @match        https://service.capilanou.ca/TDNext/Apps/72/Tickets/TicketDet?TicketID=*
+// @match        https://service.capilanou.ca/TDNext/Apps/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=capilanou.ca
 // @grant        none
 // @updateURL    https://github.com/cshier-capu/tdx-addons/raw/main/appendThumbs.js
